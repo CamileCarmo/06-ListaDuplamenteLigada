620bb19c8b461febccb313ec5bb636dd440b3da6
camilecarmo <elimacarmo6@gmail.com>
2024-09-17 13:07:12 -0300

    muito legal!

--- ListaDuplamenteLigada/ListaDuplamenteLigada/ListaDuplamenteLigada.cpp
+++ ListaDuplamenteLigada/ListaDuplamenteLigada/ListaDuplamenteLigada.cpp
@@ -150,17 +150,58 @@ void inserirElemento()
 // funções a serem implementadas no exericio
 void exibirReverso()
 {
-
+	if (primeiro == NULL) {
+		cout << "Lista vazia \n";
+		return;
+	}
+	else {
+		cout << "Elementos: \n";
+		NO* aux = ultimo;
+		while (aux != NULL) {
+			cout << aux->valor << endl;
+			aux = aux->ant;
+		}
+	}
 }
 
 void excluirPrimeiroElemento()
 {
-
+	NO* aux = primeiro;
+	if (primeiro == NULL) {
+		cout << "Lista vazia.\n" << endl;
+		return;
+	}
+	else if (primeiro == ultimo) {
+		primeiro = NULL;
+		ultimo = NULL;
+		free(aux);
+	}
+	else {
+		//O "primeiro" deve ser o "proximo do primeiro"
+		primeiro = primeiro->prox;
+		//O ponteiro anterior do "primeiro" deve apontar para nulo
+		primeiro->ant = NULL;
+		free(aux);
+	}
 }
 
 void excluirUltimoElemento()
 {
-
+	NO* aux = ultimo;
+	if (ultimo == NULL) {
+		cout << "Lista vazia.\n" << endl;
+		return;
+	}
+	else if (primeiro == ultimo) {
+		primeiro = NULL;
+		ultimo = NULL;
+		free(aux);
+	}
+	else {
+		ultimo = ultimo->ant;
+		ultimo->prox = NULL;
+		free(aux);
+	}
 }
 
 
